54240ac5121536551de852f335c09d901480d1d2
dalton_k <longlivebicycles@gmail.com>
2023-06-13 20:12:10 -0400

    Added functionality to check validity of guess and store in array

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const remainingGuessesSpan = document.querySelector("span");
 const messages = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLettersArray = [];
 
 const placeholders = function(word){
     const placeHoldersArray = [];
@@ -24,4 +25,42 @@ guessButton.addEventListener("click", function(e){
     const letterGuess = guess.value;
     console.log(letterGuess);
 guess.value="";
+messages.innerText="";
+const playerInputCheck = playerInput(letterGuess);
+console.log(playerInputCheck);
 })
+
+const playerInput = function (input){
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input===""){
+        console.log("Please enter a single letter");
+        guess.value="";
+        return;
+    }else if (input.length>1){
+        console.log("Please enter a single letter not multiple letters");
+        guess.value=""
+        return;
+    }else if (!input.match(acceptedLetter)){
+        console.log("Please enter a letter - not a number or a symbol");
+guess.value=""
+        return;
+    }else if(input!=undefined){
+        makeGuess(input);
+        return input;
+    }else{
+        console.log("Something went wrong");
+    }
+
+}
+
+const makeGuess = function(letter){
+    letter = letter.toUpperCase();
+    if (guessedLettersArray.includes(letter)){
+        console.log("You already guessed that letter");
+        return;
+    }else {
+        guessedLettersArray.push(letter);
+        console.log(guessedLettersArray);
+    }
+}
+
