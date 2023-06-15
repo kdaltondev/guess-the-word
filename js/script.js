515f504c46b66329fb13014c0919dce934113a9a
dalton_k <longlivebicycles@gmail.com>
2023-06-14 20:12:23 -0400

    Updated code

--- js/script.js
+++ js/script.js
@@ -6,11 +6,28 @@ const remainingGuessesParagraph = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector("span");
 const messages = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
-const word = "magnolia";
+let word = "magnolia";
 const guessedLettersArray = [];
 let remainingGuesses = 8;
 
 
+/*This gets a list of random words and then chooses one for the game*/
+const getWord = async function(){
+    const resp = await fetch('https://gist.githubusercontent.com/skillcrush-curriculum/7061f1d4d3d5bfe47efbfbcfe42bf57e/raw/5ffc447694486e7dea686f34a6c085ae371b43fe/words.txt');
+    const data = await resp.text();
+    console.log(data);
+    const randomWordArray = data.split("\n");
+    console.log(randomWordArray);
+    const randomNumber = Math.floor((Math.random()*randomWordArray.length));
+    const randomWord = randomWordArray[randomNumber];
+    word = randomWord.trim();
+    console.log(randomWord);
+    const currentWord = placeholders(randomWord);
+
+}
+getWord();
+
+/*Creates the placeholder array for the word that displays on the screen*/
 let placeholders = function (word) {
     const wordUpper = word.toUpperCase();
     const placeHoldersArray=[];
@@ -21,10 +38,7 @@ let placeholders = function (word) {
   return placeHoldersArray;
 };
 
-console.log(placeholders(word));
-const currentWord = placeholders(word);
-console.log(currentWord);
-
+/*Event Listener to check for guess*/
 guessButton.addEventListener("click", function (e) {
   e.preventDefault();
   const letterGuess = guess.value;
@@ -32,9 +46,9 @@ guessButton.addEventListener("click", function (e) {
   guess.value = "";
   messages.innerText = "";
   const playerInputCheck = playerInput(letterGuess);
-  console.log(playerInputCheck);
 });
 
+/*Checks to make sure the input is valid*/
 const playerInput = function (input) {
   const acceptedLetter = /[a-zA-Z]/;
   if (input === "") {
@@ -51,12 +65,12 @@ const playerInput = function (input) {
     return;
   } else if (input != undefined) {
     makeGuess(input);
-
     return input;
   } else {
     console.log("Something went wrong");
   }
 };
+
 
 const makeGuess = function (letter) {
   letter = letter.toUpperCase();
